365cb67511ebb233459d8b38042f8eeea3d9a187
bcomanici <bcomanici@nysernet.org>
2026-05-22 11:16:42 -0400

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,4 +1,17 @@
-from nautobot.apps.jobs import register_jobs
-from .automox_device_sync_job import SyncAutomoxDevices
+#from nautobot.apps.jobs import register_jobs
+#from .automox_device_sync_job import SyncAutomoxDevices
 
-register_jobs(SyncAutomoxDevices)
+#register_jobs(SyncAutomoxDevices)
+from nautobot.apps.jobs import Job, register_jobs
+
+name = "RMM Integrations"
+
+class TestRMMJob(Job):
+    class Meta:
+        name = "Test RMM Job"
+
+    def run(self):
+        self.logger.info("RMM test job loaded.")
+        return "OK"
+
+register_jobs(TestRMMJob)
